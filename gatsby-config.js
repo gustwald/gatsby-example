1241f24da26e05446982b0b69892fe4f025e4b92
Gustav Sundström <sundstrom-gustav@hotmail.com>
2018-08-23 19:56:31 +0200

    init

---
+++ gatsby-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    siteMetadata: {
+      title: `Example`
+    },
+    plugins: [
+      'gatsby-plugin-sass',
+      'gatsby-plugin-eslint',
+    ]
+  };
---
+++ src/pages/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+import style from '../styles/app.module.scss';
+
+export default () => <div className={style.app}>Hello world! </div>;
